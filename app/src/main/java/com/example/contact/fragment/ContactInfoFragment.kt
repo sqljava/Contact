f58c0f5817793c05cbb72fddc18c878ea162a971
sqljava <sqljava456@gmail.com>
2023-05-21 23:18:28 +0500

    q

--- app/src/main/java/com/example/contact/fragment/ContactInfoFragment.kt
+++ app/src/main/java/com/example/contact/fragment/ContactInfoFragment.kt
@@ -22,11 +22,7 @@ import com.example.contact.sql.DBHelper
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactInfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class ContactInfoFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
